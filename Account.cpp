8abf34ce21474e3572b4cf4dda91b20a67a52d1e
dedokok <fedaglusko@gmail.com>
2025-09-10 19:57:16 +0300

    123

---
+++ Account.cpp
@@ -0,0 +1,27 @@
+
+#include<string>;
+
+#include <nlohmann/json.hpp>
+
+class Account {
+public:
+	std::string login;
+	std::string password;
+	std::string sault;
+	int role;
+	Account() : login(""), password(""), sault(""), role(0) {} //конструктор для json
+	Account(std::string v_login, std::string v_password, std::string v_sault,int v_role) {
+		login = v_login;
+		password = v_password;
+		role = v_role;
+		sault = v_sault;
+	}
+	void set(std::string v_login, std::string v_password, std::string v_sault,int v_role) {
+		login = v_login;
+		password = v_password;
+		role = v_role;
+		sault = v_sault;
+	}
+	~Account(){}
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Account, login, password, sault, role) //чтобы можно было преобразовать json строки в объекты типа класса
+};
